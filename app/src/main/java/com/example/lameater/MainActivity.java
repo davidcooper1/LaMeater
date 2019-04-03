270eda15ec08d6715f41ce037c18937f4903dc73
WalAllie <44953580+WalAllie@users.noreply.github.com>
2019-04-03 05:22:10 -0500

    Progress on Implementing Toolbar

--- app/src/main/java/com/example/lameater/MainActivity.java
+++ app/src/main/java/com/example/lameater/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -15,6 +16,11 @@ public class MainActivity extends PermissionActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
         setCallbacks();
         obtainPermissions();
     }
