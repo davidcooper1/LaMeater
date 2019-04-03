3483058af1662330abd61550bc3f69cf67e86805
davidcooper1 <davidpcooper1@gmail.com>
2019-04-02 20:19:52 -0500

    Added temperature display to BeefOptionsActivity.

--- app/src/main/java/com/example/lameater/BeefOptionsActivity.java
+++ app/src/main/java/com/example/lameater/BeefOptionsActivity.java
@@ -4,8 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class BeefOptionsActivity extends AppCompatActivity {
+public class BeefOptionsActivity extends PermissionActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,4 +40,49 @@ public class BeefOptionsActivity extends AppCompatActivity {
 
         }
     }
+
+    protected void setCallbacks() {
+        final TemperatureFetcher fetcher = MeaterData.getInstance().getFetcher();
+        final Button tempOverview = findViewById(R.id.CurTempHomeBtn);
+
+        fetcher.setCallback(fetcher.CALLBACK_DATA_RECEIVED, new Runnable() {
+            public void run() {
+                tempOverview.post(new Runnable() {
+                    public void run() {
+                        double temp = Double.parseDouble(fetcher.getData());
+                        temp = Math.floor(temp);
+                        tempOverview.setText((int)temp + "° / --°");
+                    }
+                });
+            }
+        });
+
+        fetcher.setCallback(fetcher.CALLBACK_DISCONNECT, new Runnable() {
+            public void run() {
+                tempOverview.post(new Runnable() {
+                    public void run() {
+                        tempOverview.setText("--° / --°");
+                    }
+                });
+            }
+        });
+
+        fetcher.setCallbacksEnabled(true);
+
+
+    }
+
+    public void onPermissionGranted(int requestCode) {
+        TemperatureFetcher fetcher = MeaterData.getInstance().getFetcher();
+        Button tempOverview = findViewById(R.id.CurTempHomeBtn);
+
+        if (fetcher.getStatus() == fetcher.STATUS_DISCONNECTED) {
+            fetcher.connect();
+        } else if (fetcher.getStatus() == fetcher.STATUS_CONNECTED) {
+            double temp = Double.parseDouble(fetcher.getData());
+            temp = Math.floor(temp);
+            tempOverview.setText((int)temp + "° / --°");
+        }
+    }
+
 }
